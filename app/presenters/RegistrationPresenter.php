854be0909d38c35061307041bf4ce43182a6d167
Radek Zemanek <info@corben.cz>
2015-11-06 09:50:51 +0100

    Napojeni registrace a vytvoreni gitignore

--- app/presenters/RegistrationPresenter.php
+++ app/presenters/RegistrationPresenter.php
@@ -16,19 +16,23 @@ class RegistrationPresenter extends Nette\Application\UI\Presenter {
 	protected function createComponentRegistrationForm() {
 		$form = new UI\Form;;
 		$form->addText('us_name', 'Jméno');
+		
 		$form->addText('us_email', 'E-mail: *', 64)
 				->setEmptyValue('@')
-				->addRule(UI\Form::FILLED, 'Vyplňte Váš email')
 				->addCondition(UI\Form::FILLED)
+				->addRule(UI\Form::FILLED, 'Vyplňte Váš email')
 				->addRule(UI\Form::EMAIL, 'Neplatná emailová adresa');
+				
 		$form->addPassword('us_password', 'Heslo: *', 20)
 				->setOption('description', 'Alespoň 6 znaků')
 				->addRule(UI\Form::FILLED, 'Vyplňte Vaše heslo')
 				->addRule(UI\Form::MIN_LENGTH, 'Heslo musí mít alespoň %d znaků.', 6);
+				
 		$form->addPassword('us_password2', 'Heslo znovu: *', 20)
 				->addConditionOn($form['us_password'], UI\Form::VALID)
 				->addRule(UI\Form::FILLED, 'Heslo znovu')
-				->addRule(UI\Form::EQUAL, 'Hesla se neshodují.', $form['us_password']);
+				->addRule(UI\Form::EQUAL, 'Hesla se neshodují.', $form['us_password2']);
+				
 		$form->addSubmit('send', 'Registrovat');
 		
 		$form->onSuccess[] = array($this, 'registrationFormSubmitted');
