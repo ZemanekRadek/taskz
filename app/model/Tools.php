642dce8e43b73890cc7ad11511e10dfb24a949b6
Radek Zemanek <info@corben.cz>
2015-11-13 14:52:44 +0100

    Pridany toolsy

---
+++ app/model/Tools.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+class Tools {
+	public static function friendly_url($nadpis) {
+		$url = $nadpis;
+		$url = preg_replace('~[^\\pL0-9_]+~u', '-', $url);
+		$url = trim($url, "-");
+		$url = iconv("utf-8", "us-ascii//TRANSLIT", $url);
+		$url = strtolower($url);
+		$url = preg_replace('~[^-a-z0-9_]+~', '', $url);
+		return $url;
+	}
+}
