183a681d867202f83c377a1df23f201900377dbd
Radek Zemanek <info@corben.cz>
2015-11-13 14:52:07 +0100

    Project model and factory

--- app/model/Project.php
+++ app/model/Project.php
@@ -3,27 +3,68 @@ namespace App\Model;
 
 use Nette,
 	Nette\Database\Context,
+	Nette\Application\UI,
 	Tracy\Debugger as Debugger;
 	
 class Project extends Nette\Object  {
 	
 	/** @var Nette\Database\Context @inject */
-	private $db;
-
+	private $DB;
+	
+	private $User;
+	
+	private $table = "projects";
+	
+	private $data  = array(
+		'pr_ID'    => null,
+		'pr_name'  => null,
+		'pr_path'  => null
+	);
+	
+	public static $actualProject = null;
+	
 	/**
 	 * @param Nette\Database\Connection $db
 	 * @throws Nette\InvalidStateException
 	 */
-	public function __construct(\Nette\Database\Context $db) {
-		$this->db = $db;
+	public function __construct(
+		\Nette\Database\Context $db,
+		\App\Model\User $User,
+		$ID = null
+	) {
+		$this->DB   = $db;
+		$this->User = $User;
+		Debugger::barDump(self::$actualProject);
+		
+		if ($ID) {
+			$this->init($this->DB->table($this->table)->where('pr_ID', $ID)->fetch());
+		}
+		else if (self::$actualProject) {
+			$this->init($this->DB->table($this->table)->where('pr_ID', self::$actualProject)->fetch());
+		}
 	}
 
-	private $table = "projects";
-	
-	private $data  = array(
-		'pr_ID'   => null,
-		'pr_name' => null
-	);
+	public function & __get($name) {
+		if (in_array($name, array_keys($this->data))) {
+			return $this->data[$name];
+		}
+		
+		return parent::__get($name);
+	}
+
+	public function init($data) {
+		$keys = array_keys($this->data);
+		foreach($data as $k => $v) {
+			if (!in_array($k, $keys)) {
+				continue;
+			}
+			$this->data[$k] = $v;
+		}
+		
+		$this->data['pr_path'] = \App\Tools::friendly_url($this->data['pr_name']);
+		
+		return $this->data;
+	}
 	
 	public function getForm() {
 		$form = new UI\Form;;
@@ -39,8 +80,9 @@ class Project extends Nette\Object  {
 		return $form;
 	}
 	
-	public function save($values) {
+	public function save() {
 		
+		/*
 		$dataKeys = array_keys($this->data);
 		
 		foreach(array_keys((array) $values) as $k) {
@@ -48,21 +90,27 @@ class Project extends Nette\Object  {
 				throw Nette\InvalidArgumentException('Invalid keys for save');
 			}
 		}
+		*/
+		
+		
+		$values = clone $this->data;
+		
+		$values->pr_path = \App\Tools::friendly_url($values->pr_name);
 		
 		if ($values->pr_ID > 0) {
 
-			$this->db
+			$this->DB
 				->table($this->table)
 					->where('pr_ID', $values['pr_ID'])
 					->update($values);
 
 		}
 		else {
-			$this->db
+			$this->DB
 				->table($this->table)
 				->insert($values);
 				
-			if (!$values['pr_ID'] = $this->db->getInsertId()) {
+			if (!$values['pr_ID'] = $this->DB->getInsertId()) {
 				throw Nette\InvalidArgumentException('Invalid keys for save');
 			}
 		}
@@ -73,11 +121,16 @@ class Project extends Nette\Object  {
 	}
 	
 	public function existFromName($name) {
-		$p = $this->db
+		$p = $this->DB
 			->table($this->table)
 				->where('pr_name', $name)->fetch();
 				
 		return isset($p->pr_ID) && $p->pr_ID;
+	}
+	
+	public function loadDefault() {
+		$this->init($this->DB->table($this->table)->get(1));
+		return $this;
 	}
 	
 	public function set($ID) {
---
+++ app/model/ProjectFactory.php
@@ -0,0 +1,46 @@
+<?php
+namespace App\Model;
+
+use Nette,
+	Nette\Database\Context,
+	Tracy\Debugger as Debugger;
+	
+class ProjectFactory extends Nette\Object  {
+	
+	/** @var Nette\Database\Context @inject */
+	private $DB;
+	
+	private $User;
+	
+	private $table = 'projects';
+	
+	private $tableUser = 'projects_user';
+	
+	/**
+	 * @param Nette\Database\Connection $db
+	 * @throws Nette\InvalidStateException
+	 */
+	public function __construct(
+		\Nette\Database\Context $DB,
+		\App\Model\User $User
+	) {
+		$this->DB = $DB;
+		$this->User = $User;
+	}
+	
+	public function getAll() {
+		
+		// Debugger::barDump($this->User->getIdentity());
+		$selection = $this->DB->table($this->tableUser)
+			->where('pu_us_ID = ? ', $this->User->getIdentity()->us_ID);
+		
+		$data = array();
+		
+		foreach($selection as $list) {
+			$data[] = new \App\Model\Project($this->DB, $this->User, $list->pu_pr_ID);
+		}
+		
+		return $data;
+	}
+
+}
