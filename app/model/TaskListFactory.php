4211fd9bb064b49ab2d0944a6bee7a459ddbe239
Radek Zemanek <info@corben.cz>
2015-11-20 09:22:34 +0100

    Novy tasklist

--- app/model/TaskListFactory.php
+++ app/model/TaskListFactory.php
@@ -38,14 +38,33 @@ class TaskListFactory extends Nette\Object  {
 	
 	public function getAll() {
 		
-		// Debugger::barDump($this->User->getIdentity());
+//		Debugger::barDump($this->Project);
+	// return array();
+	
 		$selection = $this->DB->table('tasks_list_user')
 			->where('tlu_us_ID = ? ', $this->User->getIdentity()->us_ID);
 		
-		$data = array();
+		$data     = array();
+		$projects = array();
 		
 		foreach($selection as $list) {
-			$data[] = new \App\Model\TaskList($this->DB, $this->User, $this->Project, $list->tlu_tl_ID);
+			
+			foreach($this->DB->table('tasks_list_project')
+				->where('tlp_tl_ID', $list->tlu_tl_ID) as $pr) {;
+				if (!isset($projects[$list->tlu_tl_ID])) {
+					$projects[$list->tlu_tl_ID] = new \App\Model\Project($this->DB, $this->User, $pr->tlp_pr_ID);
+				}
+				
+				break;
+			}
+			
+			if ($this->Project && $pr) {
+				if ($pr->tlp_pr_ID != $this->Project->pr_ID) {
+					continue;
+				}
+			}
+				
+			$data[] = new \App\Model\TaskList($this->DB, $this->User, $projects[$list->tlu_tl_ID], $list->tlu_tl_ID);
 		}
 		
 		// Debugger::barDump($data);
