1b1d1649e0d72ac4eab5d8491328111e83d4ce15
Radek Zemanek <info@corben.cz>
2015-11-23 09:28:31 +0100

    Pridan novy systemovy identifikator pro smazane

--- app/model/TaskList.php
+++ app/model/TaskList.php
@@ -26,6 +26,7 @@ class TaskList extends Nette\Object  {
 		'inbox'     => array('tl_name' => 'Inbox', 'tl_ico'    => 'ico-inbox', 'tl_systemIdentifier'    => 'inbox', 'tl_path' => 'inbox', 'tl_color' => '0000ff'),
 		'urgent'    => array('tl_name' => 'Urgent', 'tl_ico'   => 'ico-urgent', 'tl_systemIdentifier'   => 'urgent', 'tl_path' => 'urgent', 'tl_color' => 'ff0000'),
 		'finished'  => array('tl_name' => 'Finished', 'tl_ico' => 'ico-finished', 'tl_systemIdentifier' => 'finished', 'tl_path' => 'finished', '00ff00'),
+		'deleted'   => array('tl_name' => 'Deleted', 'tl_ico' => 'ico-deleted', 'tl_systemIdentifier' => 'finished', 'tl_path' => 'deleted', '000000'),
 	);
 	
 	private $data = array(
