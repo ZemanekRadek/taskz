2ee1f8d407847a9200167f57bb17846df5d8da95
Radek Zemánek <zemanek.radek@gmail.com>
2016-10-06 12:09:31 +0200

    fix ikonky

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -4,22 +4,22 @@ namespace App\Presenters;
 use Nette,
 	Tracy\Debugger as Debugger,
 	App\Model;
-	
+
 class BasePresenter extends Nette\Application\UI\Presenter {
-	
+
 	/** @var \l10nNetteTranslator\Translator @inject */
 	public $translator;
-	
+
 	public $language;
-	
+
 	public $lang;
-	
+
 	public $user;
-	
+
 	// public $project;
-	
+
 	public $DB;
-	
+
 	public function __construct(
 		\App\Model\Language $lang,
 		\Nette\Database\Context $DB
@@ -30,27 +30,27 @@ class BasePresenter extends Nette\Application\UI\Presenter {
 
 	public function startup() {
 		parent::startup();
-		
+
 		// localization
 			$this->translator->testLanguageCode(\App\Model\Language::get());
 			$this->translator->setActiveLanguageCode(\App\Model\Language::get());
 			$this->user = $this->getUser();
-			
+
 		if (!$this->user->isLoggedIn()) {
 			$this->redirect('Sign:in', array('backlink' => $this->storeRequest()));
 		}
-		
+
 		// identity
 		\App\Model\User::$identity = $this->user->getIdentity();
-		
+
 		// project
 		if ($this->getRequest()->getPresenterName() == 'List') {
 
 		}
 		// Debugger::barDump($this->project);
-		
+
 	}
-	
+
 	function beforeRender() {
 		$this->template->lang = \App\Model\Language::get();
 		$this->template->setTranslator($this->translator);
