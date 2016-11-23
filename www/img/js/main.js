bfa4f4391acc7149299a45b259749017f6fbde32
Radek Zemánek <zemanek.radek@gmail.com>
2016-11-23 08:43:03 +0100

    loader a hover efekt na account buttonech

--- www/img/js/main.js
+++ www/img/js/main.js
@@ -1,5 +1,45 @@
+
+
+/************/
+
+var Loader = new function() {
+
+	var reference = this;
+
+	var $container = $('.page-loader');
+
+	this.show = function() {
+		$container.show();
+	}
+
+	this.hide = function() {
+		$container.hide();
+	}
+
+	this.init = function() {
+		$container = $('.page-loader');
+
+		$container.show();
+
+		$(window).on('beforeunload', function() {
+			reference.show();
+		});
+	}
+}
+
+/*****/
+
 $(function () {
 	$.nette.init();
+
+	$.nette.ext({
+		before: function(response) {
+			Loader.show();
+		},
+		success: function(response) {
+			Loader.hide();
+		}
+	});
 
 	$('#button-nav-open-sidebar').click(function(E) {
 		if (!$(this).hasClass('is-open')) {
@@ -14,5 +54,9 @@ $(function () {
 			$('.sidebar').removeClass('nav-open');
 			$('.topmenu').removeClass('nav-open');
 		}
+	});
+
+	$(document).ready(function(){
+		Loader.hide();
 	})
 });
